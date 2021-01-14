01c962bf69fc9e7bd45f0db8cd86bec972b92586
Kim <kacounts@uark.edu>
2021-01-14 15:52:39 -0600

    Task 2. Changes to be more inclusive.

--- HelloWorldApp2.java
+++ HelloWorldApp2.java
@@ -1,5 +1,6 @@
 class HelloWorldApp2 {
 	public static void main(String[] args) {
-		System.out.println("Hello World!"); // Display the string
+		System.out.println("Hello Universe!"); // Task 2- Be more inclusive
+		System.out.println("from Earth."); // Task 2- Be more inclusive.
 	}
 }
